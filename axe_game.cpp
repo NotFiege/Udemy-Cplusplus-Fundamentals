17c03f0034359d1ee6cb85167edb6eeec3be9f2a
NotFiege <91145413+NotFiege@users.noreply.github.com>
2025-02-11 10:20:52 -0600

    Update axe_game.cpp

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,25 +4,12 @@
 int main ()
 {
 
-    // false
-    bool equal = (4 == 9);
-    
-    // true
-    bool notEqual = (4 != 9);
-    
-    // true
-    bool less = (4 < 9);
-    
-    // false
-    bool greater = (4 > 9);
-    
-    // true
-    bool lessEqual = (4 <= 9);
-    
-    // false
-    bool greaterEqual = (4 >= 9);
-
     int width = 350;
     InitWindow(width, 100, "Eddie's Window");
+
+    while(true)
+    {
+        
+    }
 
 }
