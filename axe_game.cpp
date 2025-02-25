7204d1179f1af356fcee8630c1a41c174a415830
NotFiege <91145413+NotFiege@users.noreply.github.com>
2025-02-25 10:17:15 -0600

    Update axe_game.cpp

--- axe_game.cpp
+++ axe_game.cpp
@@ -3,15 +3,39 @@
 
 int main ()
 {
+    // Window Dimensions
 
     int width = 350;
     int length = 200;
     InitWindow(width, length, "Eddie's Window");
 
+    // Circle Coordinates
+
+    int circle_x = 175;
+    int circle_y = 100;
+
+    SetTargetFPS(60);
+    
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+
+        // Game Logic Begins
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            circle_x = circle_x + 10;
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            circle_x = circle_x - 10;
+        }
+
+        // Game Logic Ends
         EndDrawing();
     }
 
